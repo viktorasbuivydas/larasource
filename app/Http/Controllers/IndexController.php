ba62ea0e606df867136e47907335cb7a82e29bca
viktorasbuivydas <viktorasss00@gmail.com>
2025-02-05 21:40:35 +0200

    Added 60 s caching

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Repository;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Cache;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        $repositories = Repository::query()
-            ->with(['owners', 'licenses'])
-            ->paginate(20);
+        $repositories = Cache::remember('repositories', 60, function () {
+            return Repository::query()
+                ->with(['owners', 'licenses'])
+                ->paginate(20);
+        });
 
         return inertia('Index', [
             'repositories' => $repositories
